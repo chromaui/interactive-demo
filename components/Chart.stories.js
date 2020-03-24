8acd82f5950cc9f6ad17abfe36138c456e7e3fff
Kyle Suss <susskyle@gmail.com>
2020-03-24 11:45:39 -0600

    Add warning for docs

--- components/Chart.stories.js
+++ components/Chart.stories.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Chart from './Chart';
 
+// WARNING: Best not to change anyting in here. The place that imports this
+// component depends on the values defined here (title, story name, etc.) so
+// changing that will break the integration.
 export default {
   title: 'Chart',
 };
