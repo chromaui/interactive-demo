18a830f77a93bfde374eb7d1faf339c2d9e7eb97
Kyle Suss <susskyle@gmail.com>
2020-03-24 12:38:53 -0600

    Add target for ie in babel

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = api => {
   api.cache(true);
 
   return {
-    presets: ['@babel/env', '@babel/react'],
+    presets: [['@babel/env', { targets: { ie: '11' } }], '@babel/react'],
     plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
     env: {
       test: {
