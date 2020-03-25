c750695ff4893415c4a417b59c03f1f7c7052635
domyen <dom@hichroma.com>
2020-03-25 12:00:48 -0400

    Do not center if it's snapshotting in Chromatic

--- components/Chart.stories.js
+++ components/Chart.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Chart from './Chart';
+import isChromatic from 'storybook-chromatic/isChromatic';
 
 // WARNING: Best not to change anyting in here. The place that depends on this
 // component in the Chromatic codebase depends on the values defined here
@@ -8,18 +9,21 @@ export default {
   title: 'Chart',
   component: Chart,
   decorators: [
-    storyFn => (
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          minHeight: '100vh',
-        }}
-      >
+    storyFn =>
+      isChromatic() ? (
         <div style={{ height: '300px', width: '100%', maxWidth: 800 }}>{storyFn()}</div>
-      </div>
-    ),
+      ) : (
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: '100vh',
+          }}
+        >
+          <div style={{ height: '300px', width: '100%', maxWidth: 800 }}>{storyFn()}</div>
+        </div>
+      ),
   ],
 };
 
